eefc4cb9ed60c1ad40724b67a98d3172027a0ec9
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 15:07:34 +0000

    Feat: Added LikeList view with list and create - functionality.

--- likes/views.py
+++ likes/views.py
@@ -4,3 +4,11 @@ from .models import Like
 from .serializers import LikeSerializer
 
 # Create your views here.
+class LikeList(generics.ListCreateAPIView):
+
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
