eb3122a18c487a03c40179d1a06325b1d95dd1b1
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 14:40:06 +0000

    Feat: Added PostSerializer with fields for - ownership, likes, comments, and profile details.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,13 @@
 from rest_framework import serializers
 from likes.models import Like
 from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
+    likes_total = serializers.ReadOnlyField()
+    comments_total = serializers.ReadOnlyField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
