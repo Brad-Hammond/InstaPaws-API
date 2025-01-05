4da1b4dd8849e6f3a15979e06340499836e85e18
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 13:39:47 +0000

    Feat: Added ProfileSerializer with fields for - ownership, following status, and totals.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,3 +1,27 @@
 from rest_framework import serializers
 from followers.models import Follower
 from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+ 
+    owner = serializers.ReadOnlyField(source='owner.username')
+    following_id = serializers.SerializerMethodField()
+    posts_total = serializers.ReadOnlyField()
+    followers_total = serializers.ReadOnlyField()
+    following_total = serializers.ReadOnlyField()
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_following_id(self, obj):
+
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
