e14d8590ae3f246d6f8f658b2d7fec1ccc6511d5
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 14:50:52 +0000

    Feat: Added imports for Comment views with - permissions, filtering, and serializers.

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from instapaws_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
 # Create your views here.
