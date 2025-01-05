395c28214939d0eedb4a7f51431f153227a1a849
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 14:32:40 +0000

    Feat: Added Post model with owner, tags, - content, and image fields.

--- posts/models.py
+++ posts/models.py
@@ -2,3 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+
+    tags_array = [
+        ('Puppies', 'Puppies'),
+        ('Training', 'Training'),
+        ('Health', 'Health'),
+        ('Grooming', 'Grooming'),
+        ('Adoption', 'Adoption'),
+        ('Nutrition', 'Nutrition'),
+        ('Toys', 'Toys'),
+        ('Walks', 'Walks'),
+    ]
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    tags = models.CharField(max_length=30, choices=tags_array)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_tvgfiq', blank=True
+    )
