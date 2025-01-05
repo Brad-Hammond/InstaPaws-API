28d902454cc375712fd5b3aa1d12b5ccdaaa5fdb
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 13:25:57 +0000

    Feat: Added Profile views with filtering,- permissions, and serializers.

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from instapaws_api.permissions import IsOwnerOrReadOnly
+from .models import Profile
+from .serializers import ProfileSerializer
+
+
+# Create your views here.
