dea2f0d788e28c49b442072b69925de82b1e8d8c
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 14:25:57 +0000

    Feat: Added imports for Post views with - permissions, filtering, and serialization.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
+from instapaws_api.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
