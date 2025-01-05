8033cfbeadd9bfacf4b8ea8801315a82db0843be
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 15:08:00 +0000

    Docs: Added comment to LikeList.

--- likes/views.py
+++ likes/views.py
@@ -5,7 +5,16 @@ from .serializers import LikeSerializer
 
 # Create your views here.
 class LikeList(generics.ListCreateAPIView):
+    """
+    API view for listing and creating likes.
 
+    - serializer_class: Uses LikeSerializer for Like objects.
+    - permission_classes: Authenticated users can create; others have read-only access.
+    - queryset: Retrieves all Like instances.
+
+    Methods:
+    - perform_create: Sets the owner field to the authenticated user when creating a like.
+    """
     serializer_class = LikeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Like.objects.all()
