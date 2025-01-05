8605a2953afcd44ed61982431789e74dae239317
Brad-Hammond <brad_hammond1@sky.com>
2025-01-05 14:26:39 +0000

    Feat: Added PostList view with annotations, - filtering, search, ordering, and create functionality.

--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,36 @@ from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
+class PostList(generics.ListCreateAPIView):
+
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.annotate(
+        likes_total=Count('likes', distinct=True),
+        comments_total=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__profile',
+        'tags',
+        'likes__owner__profile',
+        'owner__followed__owner__profile',
+    ]
+    search_fields = [
+        'title',
+        'tags',
+        'owner__username'
+    ]
+    ordering_fields = [
+        'comments_total',
+        'likes_total',
+        'likes__created_at',
+    ]
+
+    def perform_create(self, serializer):
+ 
+        serializer.save(owner=self.request.user)
