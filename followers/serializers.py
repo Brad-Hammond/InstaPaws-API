b1b2b46c0d04160d93fa89fff162e23b48d5d9a9
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:30:44 +0000

    Docs: Added comments to classes and functions - in the serializers.py file.

--- followers/serializers.py
+++ followers/serializers.py
@@ -3,17 +3,31 @@ from rest_framework import serializers
 from .models import Follower
 
 class FollowerSerializer(serializers.ModelSerializer):
-
+    """
+    FollowerSerializer:
+    - Serializes the Follower model to handle JSON data exchange.
+    - Includes fields to represent the owner (user who follows) and 
+      the followed user, along with additional metadata.
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     followed_username = serializers.ReadOnlyField(source='followed.username')
 
     class Meta:
-
+        """
+        Meta:
+        - Specifies the model associated with this serializer.
+        - Defines the fields to be serialized.
+        """
         model = Follower
         fields = ['id', 'owner', 'created_at', 'followed_username', 'followed']
 
     def create(self, validated_data):
-
+        """
+        Custom create method:
+        - Attempts to create a new Follower instance with the validated data.
+        - Handles the IntegrityError if the user tries to follow the same 
+          person more than once, raising a validation error instead.
+        """
         try:
             return super().create(validated_data)
         except IntegrityError:
