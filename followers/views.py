360f55ae0b47f3fef41a829b3f31e0a01c74f9b5
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:16:05 +0000

    Feat: Imported   necessary modules, permissions,- models, and serializers in followers app views.py.

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from instapaws_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
 # Create your views here.
