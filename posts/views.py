e7deaabf7d15d9b4ef6fd61ff63f9e205c9db7a1
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 15:11:46 +0000

    Feat: Changed api name to match application name.

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions, filters
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
-from instapaws_api.permissions import IsOwnerOrReadOnly
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
