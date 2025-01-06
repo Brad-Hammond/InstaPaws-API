a9e37606a0e16df9e7af163110ec0c824368cb40
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:03:24 +0000

    Feat: Added  like model with owner, post, and - created_at fields.

--- likes/models.py
+++ likes/models.py
@@ -3,3 +3,14 @@ from django.contrib.auth.models import User
 from posts.models import Post
 
 # Create your models here.
+class Like(models.Model):
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
