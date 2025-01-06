31f05d825fa54b3cc27ff30458fe375f5d74e5af
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 15:34:38 +0000

    Feat: Added  home_route for API welcome message- and logout_route to clear JWT cookies.

--- drf_api/views.py
+++ drf_api/views.py
@@ -5,3 +5,33 @@ from .settings import (
     JWT_AUTH_SECURE
 )
 
+@api_view()
+def home_route(request):
+
+    return Response({
+        "message": "Hello! This is InstaPaws API"
+    })
+
+@api_view(['POST'])
+def logout_route(request):
+
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1960 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1960 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
