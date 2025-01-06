9d9c4b70a21cd320572d391a4f16c88d15d67adb
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:22:25 +0000

    Docs: Added comments to follower model.

--- followers/models.py
+++ followers/models.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Follower(models.Model):
+    """
+    Represents a follower relationship between users.
 
+    - 'owner': The user who is following another user, linked to the User model.
+    - 'followed': The user being followed, also linked to the User model.
+    - 'created_at': Timestamp when the follow relationship was created.
+    """
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
