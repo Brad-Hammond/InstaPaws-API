924f44947871c8c4b04a25457f062b0cd8faac81
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 15:05:16 +0000

    Docs: Added comments to serializers.py file.

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -2,12 +2,22 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
-
+    """
+    CurrentUserSerializer:
+    - Extends the `UserDetailsSerializer` from dj_rest_auth to include additional fields.
+    - Adds fields to represent the user's profile information, specifically the profile ID
+      and the URL of the profile image.
+    """
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
-
+        """
+        Meta:
+        - Extends the Meta class of the base `UserDetailsSerializer`.
+        - Adds custom fields `profile_id` and `profile_image` to the serialized output
+          while retaining all fields from the base serializer.
+        """
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id', 'profile_image'
         )
