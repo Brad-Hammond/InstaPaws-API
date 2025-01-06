fa1959116f3f8af6420eec5521a9e82773127ef9
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:22:59 +0000

    Feat: Added Meta class for ordering and - unique_together constraint to Follower model, and define str method.

--- followers/models.py
+++ followers/models.py
@@ -20,3 +20,12 @@ class Follower(models.Model):
         related_name='followed'
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+
+        return f'{self.owner} {self.followed}'
