69ae04ba6f5472c170df757ec26a117c88d24bbe
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:21:31 +0000

    Feat: Added Follower model with owner, followed, - and created_at fields.

--- followers/models.py
+++ followers/models.py
@@ -2,3 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Follower(models.Model):
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following')
+    followed = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followed'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
