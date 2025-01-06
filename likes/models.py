c0f624fd0f8b485fa6f788941ffb73691009cbc9
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:04:29 +0000

    Docs: Added comments to Like model.

--- likes/models.py
+++ likes/models.py
@@ -4,7 +4,16 @@ from posts.models import Post
 
 # Create your models here.
 class Like(models.Model):
+    """
+    Represents a 'like' on a post in the application.
 
+    Fields:
+    - owner: The user who liked the post, linked as a foreign key to the User model.
+             If the user is deleted, their likes will also be removed (on_delete=models.CASCADE).
+    - post: The post that has been liked, linked as a foreign key to the Post model.
+            Includes a 'related_name' attribute ('likes') to enable reverse access from a Post instance.
+    - created_at: A timestamp that indicates when the like was created, automatically set at the time of creation.
+    """
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE)
