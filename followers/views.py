98215a72af28d3a7ff4b4fff323e8be6bf0816bc
Brad-Hammond <brad_hammond1@sky.com>
2025-01-06 14:16:50 +0000

    Feat: Added FollowerList API view with List and - Create functionality, set permissions, and handle owner assignment in followers app.

--- followers/views.py
+++ followers/views.py
@@ -4,3 +4,11 @@ from .models import Follower
 from .serializers import FollowerSerializer
 
 # Create your views here.
+class FollowerList(generics.ListCreateAPIView):
+
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
